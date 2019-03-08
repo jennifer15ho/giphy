6c978ff2f4b82876278b7e232856f2778d44e9a2
Jennifer Ho <jenniferho222@gmail.com>
2019-03-08 02:30:01 +0000

    able to search for any gif

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    var input = $("#search-term").val();
+        $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?q=" + input + "&rating=pg&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+        success: function(response){
+            var pic_url = response.data[0].images.original.url;
+            $(".text-center").html('<img src="' + pic_url + '"/>');
+    
+        }
+    });
 });
 
