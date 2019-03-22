1eccdccd390139b6d54853e9010ebb126ed7c3bf
Jennifer Ho <jenniferho222@gmail.com>
2019-03-22 00:57:47 +0000

    modal

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
+var pic_url;
 $("#search-button").click(function(){
     
     var input = $("#search-term").val();
@@ -10,11 +10,8 @@ $("#search-button").click(function(){
         url: "https://api.giphy.com/v1/gifs/search?q=" + input + "&rating=pg&api_key=dc6zaTOxFJmzC",
         method: "GET",
         success: function(response){
-            
-            var pic_url = response.data.forEach(function(element)
-            {
-                return (response.data);
-            }).images.original.url;
+            var random = Math.floor(Math.random()*response.data.length);
+            pic_url = response.data[random].images.original.url;
             
             $(".text-center").html('<img src="' + pic_url + '"/>');
     
@@ -22,5 +19,17 @@ $("#search-button").click(function(){
     });
 });
 
-//var random = Math.floor(Math.random()*response.data.length);
-//response.data[random].images.original.url
+$(".text-center").click(function(){
+   $(".dark").css("display", "block");
+  $(".modal").append('<img src="' + pic_url + '"/>');
+  $(".modal").css("display", "block");
+});
+
+$(".close").click(function(){
+    $(".modal").css("display","none");
+    $(".dark").css("display","none");
+});
+
+
+//
+//
